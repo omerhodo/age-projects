0ce6a8226c2397277aea8bce9976f7c747285c98
Ömer Hodo <omerhodo@gmail.com>
2025-07-25 15:30:20 +0300

    add useplatform hooks and update some styles

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,8 +5,8 @@ const config: CapacitorConfig = {
   appName: 'Age Calculator',
   webDir: 'out',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
---
+++ src/hooks/index.ts
@@ -0,0 +1,1 @@
+export { usePlatform, type PlatformInfo } from './usePlatform';
