25b4376a23db70b121b599700041319790572397
Ömer Hodo <omerhodo@gmail.com>
2025-07-25 14:50:03 +0300

    add capacitorjs

---
+++ android/app/src/main/java/com/omerhodo/agecalculator/MainActivity.java
@@ -0,0 +1,5 @@
+package com.omerhodo.agecalculator;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.omerhodo.agecalculator',
+  appName: 'Age Calculator',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
